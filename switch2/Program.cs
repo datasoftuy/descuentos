cadfcbc16707a18d39046726446ea73c4db58c03
datasoftuy <andres.esteves.aharonian@gmail.com>
2017-05-10 21:24:19 -0300

    finalizado

--- switch2/Program.cs
+++ switch2/Program.cs
@@ -3,79 +3,132 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace switch2
+namespace leapyear
 {
     class Program
     {
         static void Main(string[] args)
         {
-        
-        
-        
-        // pedir un precio y una categoria segun la categoria el precio recibira un aumewnto o descuento
-        //CatA 10 CatB 20  CatC 10  
-
-        // CatD 15% aumento si el precio es menor a 600 y sino 20%
 
 
 
 
-            float precio;
-            double descuento,preciofinal;
-            string categoria;
-            Console.WriteLine("Ingrese un precio");
-            precio = Convert.ToInt64(Console.ReadLine());
-
-
-            Console.WriteLine("Ingrese la categoria");
-            
-            categoria = Console.ReadLine().ToUpper();
-
-
-            switch (categoria)
-
-
+            bool salir = false;
+            string otravez;
+            while (salir != true)
             {
 
-                 case "A":
+
+                // pedir un precio y una categoria segun la categoria el precio recibira un aumewnto o descuento
+                //CatA 10 CatB 20  CatC 10  
+
+                // CatD 15% aumento si el precio es menor a 600 y sino 20%
 
 
+
+                Console.Clear();
+                float precio;
+                double descuento, preciofinal, impuesto;
+                string categoria;
+                Console.WriteLine("Ingrese un precio");
+                precio = Convert.ToInt64(Console.ReadLine());
+
+
+                Console.WriteLine("Ingrese la categoria");
+
+                categoria = Console.ReadLine().ToUpper();
+
+
+                switch (categoria)
+                {
+
+                    case "A":
+
+                    case "C":
                         descuento = precio * 0.10;
                         preciofinal = precio - descuento;
                         Console.WriteLine("El precio final es: " + preciofinal);
-                        Console.ReadKey();
-
+                        Console.ReadLine();
                         break;
 
-
-                case "B":
+                    case "B":
 
                         descuento = precio * 0.20;
                         preciofinal = precio - descuento;
                         Console.WriteLine("El precio final es: " + preciofinal);
-                        
+                        Console.ReadLine();
+
 
                         break;
 
-                 default: Console.Write("Categoria incorrecta");
-                        Console.ReadKey();
-                break;
+                    case "D":
+
+                        if (precio < 600)
+                        {
+
+                            impuesto = precio * 0.15;
+
+                            preciofinal = precio + impuesto;
+
+                            Console.WriteLine("El precio final es: " + preciofinal);
+
+                        }
+
+
+                        else
+                        {
+
+                            impuesto = precio * 0.20;
+
+                            preciofinal = precio + impuesto;
+
+                            Console.WriteLine("El precio final es: " + preciofinal);
+
+
+                        }
+
+                        Console.ReadLine();
+
+
+                        break;
+
+                    default: Console.Write("Categoria incorrecta");
+                        Console.ReadLine();
+                        break;
 
 
 
 
 
-                
-            
-            
-            
-            
-            
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+                Console.WriteLine("Desea calcular otro precio s/n");
+                otravez = Console.ReadLine().ToLower();
+                if (otravez == "n")
+                {
+                    salir = true;
+                }
             }
 
 
 
-        
+
+
+
+
+
+
         }
     }
 }
